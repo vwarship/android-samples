312470f889686456aa32fdcae06015eb2af20d61
王军建 <vwarship@163.com>
2014-02-02 13:51:24 +0800

    增加了可视化观察Activity生命周期。

--- ActivityLifecycle/src/com/vwarship/activitylifecycle/MainActivity.java
+++ ActivityLifecycle/src/com/vwarship/activitylifecycle/MainActivity.java
@@ -4,12 +4,19 @@ import com.vwarship.activitylifecycle.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private static final String TAG = "MainActivity";
+	
+	private Button m_btnSearch;
+	private Button m_btnDesktop;
+	private Button m_btnClear;
 	
 	private TextView m_textView;
 	
@@ -17,6 +24,36 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //onPause -> onResume
+        m_btnSearch = (Button)this.findViewById(R.id.btnSearch);
+        m_btnSearch.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent();
+				intent.setClassName("com.android.quicksearchbox", "com.android.quicksearchbox.SearchActivity");
+				startActivity(intent);
+			}
+		});
+        
+        //onPause -> onStop -> onRestart -> onStart -> onResume
+        m_btnDesktop = (Button)this.findViewById(R.id.btnDesktop);
+        m_btnDesktop.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(Intent.ACTION_MAIN);
+				intent.addCategory(Intent.CATEGORY_HOME);
+				startActivity(intent);
+			}
+		});
+        
+        m_btnClear = (Button)this.findViewById(R.id.btnClear);
+        m_btnClear.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				m_textView.setText("");
+			}
+		});
         
         m_textView = (TextView)this.findViewById(R.id.textView);
         
