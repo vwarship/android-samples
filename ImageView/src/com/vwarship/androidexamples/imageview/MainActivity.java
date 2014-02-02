a8f00d3c53c347a4917745bfd417262d5a716198
王军建 <vwarship@163.com>
2014-02-02 22:02:28 +0800

    ImageView

---
+++ ImageView/src/com/vwarship/androidexamples/imageview/MainActivity.java
@@ -0,0 +1,17 @@
+package com.vwarship.androidexamples.imageview;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		//ImageView imageView = (ImageView)this.findViewById(R.id.imageview);
+		//imageView.setLayoutParams(new LinearLayout.LayoutParams(300, 200));
+	}
+
+}
